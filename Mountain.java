c90b70fa92edc7a25df1f1f994e2389d2f3ce298
VISHAL SINGH RANA <115576294+ranavishal0002@users.noreply.github.com>
2024-04-21 00:16:18 +0530

    Update Mountain.java

--- Mountain.java
+++ Mountain.java
@@ -1,3 +1,17 @@
+This is the leetcode problem and here is the description
+
+# An array arr is a mountain if the following properties hold:
+
+arr.length >= 3
+There exists some i with 0 < i < arr.length - 1 such that:
+arr[0] < arr[1] < ... < arr[i - 1] < arr[i] 
+arr[i] > arr[i + 1] > ... > arr[arr.length - 1]
+Given a mountain array arr, return the index i such that arr[0] < arr[1] < ... < arr[i - 1] < arr[i] > arr[i + 1] > ... > arr[arr.length - 1].
+
+You must solve it in O(log(arr.length)) time complexity.
+
+#solution
+
 class Solution {
     public int peakIndexInMountainArray(int[] arr) {
         if (arr.length < 3) {
@@ -7,7 +21,7 @@ class Solution {
         for (int i = 0; i < arr.length - 1; i++) {
             if (arr[i] < arr[i + 1]) {
                 result = i + 1;
-            }
+            }e 
         }
         return result;
     }
